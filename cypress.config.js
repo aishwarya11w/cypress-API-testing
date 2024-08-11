11cb76ce3063a208ef24c4d6f7f552b19f4491bc
aishwaryaw57 <aishwaryawarulkar999@gmail.com>
2024-08-11 12:53:19 +0530

    API Testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
